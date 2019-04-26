670b38e8f50b93a46c92081a8b52daf8e2093fdf
Felipe Carneiro <felipedossantos896@gmail.com>
2019-04-26 07:18:02 -0300

    Adicionando o header da página

---
+++ site-dinamico-01/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras,chave,do,meu,site">
+    <meta name="description" content="Descrição do meu site">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projeto 01</title>
+</head>
+<body>
+    
+    <header>
+        <div class="logo">Logomarca</div><!--logo-->
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+        <nav class="mobile">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+
+</body>
+</html>
