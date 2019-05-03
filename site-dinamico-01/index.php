d666deb8a2505713a62dffe6f51accc6eaf40533
Felipe Carneiro <felipedossantos896@gmail.com>
2019-05-03 18:45:53 -0300

    Adicionando awesome icons e google fonts

--- site-dinamico-01/index.php
+++ site-dinamico-01/index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="estilo/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="palavras,chave,do,meu,site">
