772da1b9ee7fdbbdc6d2c01e55615e2db911c13b
Felipe Carneiro <felipedossantos896@gmail.com>
2019-05-03 07:44:06 -0300

    Adicionando div.center

--- site-dinamico-01/index.php
+++ site-dinamico-01/index.php
@@ -11,47 +11,60 @@
 <body>
     
     <header>
-        <div class="logo">Logomarca</div><!--logo-->
-        <nav class="desktop">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav><!-- Fim do nav.desktop -->
-        <nav class="mobile">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav><!-- Fim do nav.mobile -->
+        <div class="center">
+            <div class="logo">Logomarca</div><!--logo-->
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav><!-- Fim do nav.desktop -->
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav><!-- Fim do nav.mobile -->
+        </div><!-- center -->
     </header><!-- Fim do header -->
     
     <section class="banner-principal"><!-- banner-principal-->
-        <form>
-            <h2>Qual o seu melhor e-mail?</h2>
-            <input type="email" name="email" required />
-            <input type="submit" name="acao" value="Cadastrar!">
-        </form>
+        <div class="center">
+            <form>
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name="email" required />
+                <input type="submit" name="acao" value="Cadastrar!">
+            </form>
+        </div>
     </section><!-- Fim do banner-principal -->
 
     <section class="descricao-autor"><!-- descricao-autor -->
-        <div class="w50">
-            <h2>Felipe S. Carneiro</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </div>
-        
-        <div class="w50">
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </div>
-        <div class="w50">
-            <!-- Pegar imagem depois -->
-            <img src="images/foto.jpg" alt="">
-        </div><!-- w50 -->
+        <div class="center">
+            <div class="w50">
+                <h2>Felipe S. Carneiro</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+            
+            <div class="w50">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+            <div class="w50">
+                <!-- Pegar imagem depois -->
+                <img src="images/foto.jpg" alt="">
+            </div><!-- w50 -->
+        </div><!-- center -->
     </section><!-- descricao-autor -->
+
+    <section class="especialidades">
+        <h2 class="title">Especialidades</h2>
+        <div class="center">
+        
+        </div><!-- center -->
+    </section> <!-- especialidades -->
 
 </body>
 </html>
